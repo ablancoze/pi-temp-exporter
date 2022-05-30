0c268ee51e76451f27c82a19f2b027ba84d15be8
ablancom <ablancom@kyndryl.com>
2022-05-30 16:26:25 +0200

    config script to initializate the env

--- temp-exporter.bash
+++ temp-exporter.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
-sudo apt install python3 -y
+sudo apt install python3 git -y
 python -m pip3 install --upgrade pip
 pip3 install pyspectator datetime
+cd $HOME
+git clone https://github.com/ablancoze/pi-temp-exporter.git 
+cd pi-temp-exporter
+python3 read-temp.py
+*/5 * * * * python3 $HOME/pi-temp-exporter/read-temp.py start
